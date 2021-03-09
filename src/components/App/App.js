0127b49eec15ece6dfabc330f39c8085b671ca32
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-03-09 09:48:25 -0700

    Add useEffect to fetch async

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { getUrls } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      urls: []
+const App = () => {
+  const [urls, setUrls] = useState([])
+  const [appError, setAppError] = useState()
+
+  async function fetchData() {
+    try {
+      const data = await getUrls()
+      setUrls(data)
+    } catch (error) {
+      setAppError(error)
     }
   }
 
-  componentDidMount() {
-  }
-
-  render() {
-    return (
+  useEffect(async () => {
+    fetchData()
+  }, [])
+    
+  return (
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
           <UrlForm />
         </header>
-
-        <UrlContainer urls={this.state.urls}/>
+        {appError && <p>{appError}</p>}
+        <UrlContainer urls={urls}/>
       </main>
     );
-  }
 }
 
 export default App;
