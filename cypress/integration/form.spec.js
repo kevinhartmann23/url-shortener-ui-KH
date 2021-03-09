15793bf02dc7a73106fe15d2f71e6668b6881fec
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-03-09 11:03:55 -0700

    Add testing structure

---
+++ cypress/integration/form.spec.js
@@ -0,0 +1,33 @@
+const baseUrl = 'http://localhost:3000/'
+const apiUrl = 'http://localhost:3001/api/v1/urls'
+
+describe('Home Page', () => {
+  it('should visit home page', () => {
+    cy
+      .visit(baseUrl)
+  })
+
+  it('should', () => {
+
+  })
+
+  it('should', () => {
+
+  })
+
+  it('should', () => {
+
+  })
+
+  it('should', () => {
+
+  })
+
+  it('should', () => {
+
+  })
+
+  it('should', () => {
+
+  })
+})
